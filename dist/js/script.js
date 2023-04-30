808181704267041b39fe2fc5da8992b793750ee6
Yuliia Murlina <y4liamur@gmail.com>
2023-04-30 13:25:57 +0200

    added fetch

--- dist/js/script.js
+++ dist/js/script.js
@@ -309,25 +309,25 @@ document.addEventListener("DOMContentLoaded", () => {
       } //Создание запроса на сервер
 
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-      request.setRequestHeader("Content-type", "application/json; charset=utf-8");
       const formData = new FormData(form);
       const object = {};
       formData.forEach((value, key) => {
         object[key] = value;
       });
-      const json = JSON.stringify(object);
-      request.send(json);
-      request.addEventListener("load", () => {
-        if (request.status == 200) {
-          console.log(request.response);
-          showThanksModal(message.success);
-          form.reset();
-          statusMessage.remove();
-        } else {
-          showThanksModal(message.fail);
-        }
+      fetch("server.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(object)
+      }).then(data => data.text()).then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.fail);
+      }).finally(() => {
+        form.reset();
       });
     });
   }
--- src/js/main.js
+++ src/js/main.js
@@ -261,31 +261,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
       //Создание запроса на сервер
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      );
       const formData = new FormData(form);
       const object = {};
       formData.forEach((value, key) => {
         object[key] = value;
       });
-      const json = JSON.stringify(object);
-      request.send(json);
 
-      request.addEventListener("load", () => {
-        if (request.status == 200) {
-          console.log(request.response);
+      fetch("server.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(object),
+      })
+        .then((data) => data.text())
+        .then((data) => {
+          console.log(data);
           showThanksModal(message.success);
-
-          form.reset();
           statusMessage.remove();
-        } else {
+        })
+        .catch(() => {
           showThanksModal(message.fail);
-        }
-      });
+        })
+        .finally(() => {
+          form.reset();
+        });
     });
   }
 
